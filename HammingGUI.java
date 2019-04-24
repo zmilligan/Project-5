beb097abc8c2bb515e2ced09bff055178a7aafbb
Zandi Milligan <zmilligan@ou.edu>
2019-04-24 17:24:24 -0500

    added more components

--- HammingGUI.java
+++ HammingGUI.java
@@ -1,31 +1,109 @@
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
+import javax.swing.JTextField;
 
 public class HammingGUI extends JFrame
 {
 	private JPanel panel1 = new JPanel();
 	private JPanel panel2 = new JPanel();
 	private JSlider slider;
+	private JLabel sLabel;
+	private JTextField sText;
+	private JTextField dist0;
+	private JTextField dist1;
+	private JTextField dist2;
+	private JTextField dist3;
+	private JTextField dist4;
+	private JButton show;
 	
 	public HammingGUI()
 	{
 		this.setSize(600, 700);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		GridLayout gl = new GridLayout(0, 2);
-		
+		GridBagLayout gbl = new GridBagLayout();
+		GridBagConstraints gbc = new GridBagConstraints();
+		this.setLayout(gl);
+		panel1.setLayout(gbl);
 		
 		setSlider();
-		panel1.add(slider);
+		setLabels();
+		setTextFields();
+		setButtons();
 		
-		this.setLayout(gl);
+		addComponentsPanel1(gbc);
+		
+		
 		this.add(panel1);
 		this.add(panel2);
 		this.setVisible(true);
 	}
 	
+	private void setButtons()
+	{
+		show = new JButton("Show Station");
+	}
+
+	private void addComponentsPanel1(GridBagConstraints gbc) 
+	{
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.gridx = 0; gbc.gridy = 0;
+        panel1.add(sLabel, gbc);
+ 
+        gbc.gridx = 1; gbc.gridy = 0;
+        panel1.add(sText, gbc);
+ 
+        gbc.gridx = 0; gbc.gridy = 2;
+        //gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.gridwidth = 2; gbc.gridheight = 2;
+        panel1.add(slider, gbc);
+        
+        gbc.fill = GridBagConstraints.VERTICAL;
+        
+        gbc.gridx = 0; gbc.gridy = 6;
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+        panel1.add(show, gbc);
+        
+        gbc.gridx = 0; gbc.gridy = 7;
+        panel1.add(dist0, gbc);
+		
+	}
+
+	private void setTextFields() 
+	{
+		sText = new JTextField();
+		sText.setEditable(false);
+		sText.setText(" " + slider.getValue());
+		
+		dist0 = new JTextField();
+		dist0.setEditable(false);
+		
+		dist1 = new JTextField();
+		dist1.setEditable(false);
+		
+		dist2 = new JTextField();
+		dist2.setEditable(false);
+		
+		dist3 = new JTextField();
+		dist3.setEditable(false);
+		
+		dist4 = new JTextField();
+		dist4.setEditable(false);
+	}
+
+	private void setLabels() 
+	{
+		sLabel = new JLabel("Enter Hamming Dist: ");
+	}
+
 	private void setSlider()
 	{
 		slider = new JSlider(0, 1, 4, 1);
