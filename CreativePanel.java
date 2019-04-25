6ad75cf4eac388f37ab5e9fd9de1aed3ce37f22e
Zandi Milligan <zmilligan@ou.edu>
2019-04-25 12:35:39 -0500

    created skeleton

--- CreativePanel.java
+++ CreativePanel.java
@@ -1,4 +1,16 @@
+import javax.swing.JPanel;
 
-public class CreativePanel {
-
+public class CreativePanel
+{
+	private JPanel panel = new JPanel();
+	
+	public CreativePanel()
+	{
+		
+	}
+	
+	public JPanel getPanel()
+	{
+		return panel;
+	}
 }
